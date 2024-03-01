be8a56e67127a3a2530e95c83d551389bee781e6
David-Saini <lakshaysaini92558@gmail.com>
2024-03-01 10:41:09 +0530

    onclick function in navbar

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -24,11 +24,10 @@ const BackToTop = () => {
     })
     return (
         <>
-
             <span
                 onClick={() => setPosition({ ...position, position: { top: 0, left: 0 } })} className="circle"
-                ref={scrollTop}><img className='w-full flex' src={topUp} alt="topUp" /></span>
-
+                ref={scrollTop}><img className='w-full flex' src={topUp} alt="topUp" />
+            </span>
         </>
 
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,6 +7,14 @@ import CrossIcon from "../assets/images/png/CrossIcon.png"
 const Nav = () => {
     const [Menu, setMenu] = useState(false);
 
+    const toggleMenu = () => {
+        setMenu(!Menu);
+    };
+
+    const closeMenu = () => {
+        setMenu(false);
+    };
+
     if (Menu) {
         document.body.classList.add("overflow-hidden");
     }
@@ -25,7 +33,7 @@ const Nav = () => {
                         <div className={`${Menu ? 'show-nav' : 'nav-fix'}`}>
                             <div className='z-index-100 relative' onClick={() => setMenu}>
                                 <div className='absolute top-[20px] right-[20px]'>
-                                    <Link><img className='w-[18px]' src={CrossIcon} alt="CrossIcon" /></Link>
+                                    <Link><img className='w-[18px]' src={CrossIcon} alt="CrossIcon" onClick={toggleMenu} /></Link>
                                 </div>
                             </div>
                         </div>
@@ -34,17 +42,17 @@ const Nav = () => {
                             <Link><img className='w-[99px] sm:w-[120px] md:w-[149px] absolute top-[15px] left-[15px]' src={HeaderLogo} alt="HeaderLogo" /></Link>
                             <div className='z-index-100' onClick={() => setMenu(false)}>
                                 <div className='absolute top-[20px] right-[15px]'>
-                                    <Link><img className='w-[18px]' src={CrossIcon} alt="CrossIcon" /></Link>
+                                    <Link><img className='w-[18px]' src={CrossIcon} alt="CrossIcon" onClick={toggleMenu} /></Link>
                                 </div>
                             </div>
 
                             <div className='flex items-center justify-center flex-col'>
                                 <div className='bg-white opacity-20 h-[1px] w-full px-5 mt-8 mb-6'></div>
                                 <ul className='flex align-center justify-center ps-0 mb-0'>
-                                    <li className='relative'><a href='#Services' className='text-white hover:text-[#ffa852] duration-300 font-medium ff_CircularStd leading-[126.5%] text-base mobile_navlinks'>Menu</a></li>
-                                    <li className='relative'><a href='#Chat' className='text-white hover:text-[#ffa852] duration-300 font-medium ff_CircularStd leading-[126.5%] text-base mobile_navlinks'>Menu</a></li>
-                                    <li className='relative'><a href='#Comunity' className='text-white hover:text-[#ffa852] duration-300 font-medium ff_CircularStd leading-[126.5%] text-base mobile_navlinks'>Menu</a></li>
-                                    <li className='relative'><a href='#Pricing' className='text-white hover:text-[#ffa852] duration-300 font-medium ff_CircularStd leading-[126.5%] text-base mobile_navlinks'>Menu</a></li>
+                                    <li className='relative'><a href='#Services' className='text-white hover:text-[#ffa852] duration-300 font-medium ff_CircularStd leading-[126.5%] text-base mobile_navlinks' onClick={closeMenu}>Menu</a></li>
+                                    <li className='relative'><a href='#Chat' className='text-white hover:text-[#ffa852] duration-300 font-medium ff_CircularStd leading-[126.5%] text-base mobile_navlinks' onClick={closeMenu}>Menu</a></li>
+                                    <li className='relative'><a href='#Comunity' className='text-white hover:text-[#ffa852] duration-300 font-medium ff_CircularStd leading-[126.5%] text-base mobile_navlinks' onClick={closeMenu}>Menu</a></li>
+                                    <li className='relative'><a href='#Pricing' className='text-white hover:text-[#ffa852] duration-300 font-medium ff_CircularStd leading-[126.5%] text-base mobile_navlinks' onClick={closeMenu}>Menu</a></li>
                                 </ul>
                                 <div className='block lg:hidden mt-4'>
                                     <button className='ff_CircularStd leading-[126.5%] text-base text-white w-[335px] h-[50px] rounded-[10px] bg-[#FFA852] px-[37px] border border-solid border-[#ffa852] hover:border-black hover:bg-black duration-300 font-medium'>Login</button>
